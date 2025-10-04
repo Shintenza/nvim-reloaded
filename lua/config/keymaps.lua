52c9466e555180297401f0a6c56a9c4a57e32018
Kamil Kuziora <kam.kuziora@gmail.com>
2025-10-04 22:14:52 +0200

    feat: indent detection, better buffer bindings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,21 +1,22 @@
 local opts = { noremap = false, silent = true }
 
 function keymap(mode, keys, action, description)
-  local binding_options = {}
-  for k, v in pairs(opts) do
-    binding_options[k] = v
-  end
-  if description ~= nil then
-    binding_options["desc"] = description
-  end
-  vim.keymap.set(mode, keys, action, binding_options)
+	local binding_options = {}
+	for k, v in pairs(opts) do
+		binding_options[k] = v
+	end
+	if description ~= nil then
+		binding_options["desc"] = description
+	end
+	vim.keymap.set(mode, keys, action, binding_options)
 end
 
-keymap("n", "<leader-[>", "<cmd>BufferLineCyclePrev<CR>", "Next buffer")
-keymap("n", "<leader-]>", "<cmd>BufferLineCycleNext<CR>", "Prev buffer")
+keymap("n", "<A-x>", ":bd!<CR>", "Close buffer")
+keymap("n", "<A-[>", "<cmd>BufferLineCyclePrev<CR>", "Next buffer")
+keymap("n", "<A-]>", "<cmd>BufferLineCycleNext<CR>", "Prev buffer")
 
 keymap("n", "<leader>e", ":NvimTreeToggle<CR>", "Toggle file explorer")
-keymap("n", "<leader>z", ":ToggleTerm<CR>", "Toggle terminal")
+keymap("n", "<leader>t", ":ToggleTerm<CR>", "Toggle terminal")
 
 local telescope = require("telescope.builtin")
 keymap("n", "<leader>ff", telescope.find_files, "Telescope find files")
@@ -28,28 +29,28 @@ keymap("t", "<esc>", [[<C-\><C-n>]])
 local M = {}
 
 function M.get_lsp_bindigs(event)
-  local bufmap = function(mode, rhs, lhs)
-    vim.keymap.set(mode, rhs, lhs, { buffer = event.buf })
-  end
+	local bufmap = function(mode, rhs, lhs)
+		vim.keymap.set(mode, rhs, lhs, { buffer = event.buf })
+	end
 
-  -- Standard lsp mappings
-  bufmap("n", "gra", "<cmd>lua vim.lsp.buf.code_action()<cr>")
-  bufmap("n", "gO", "<cmd>lua vim.lsp.buf.document_symbol()<cr>")
-  bufmap({ "i", "s" }, "<C-s>", "<cmd>lua vim.lsp.buf.signature_help()<cr>")
+	-- Standard lsp mappings
+	bufmap("n", "gra", "<cmd>lua vim.lsp.buf.code_action()<cr>")
+	bufmap("n", "gO", "<cmd>lua vim.lsp.buf.document_symbol()<cr>")
+	bufmap({ "i", "s" }, "<C-s>", "<cmd>lua vim.lsp.buf.signature_help()<cr>")
 
-  bufmap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>")
-  bufmap("n", "grt", "<cmd>lua vim.lsp.buf.type_definition()<cr>")
-  bufmap("n", "grd", "<cmd>lua vim.lsp.buf.declaration()<cr>")
-  bufmap({ "n", "x" }, "gq", "<cmd>lua vim.lsp.buf.format({async = true})<cr>")
+	bufmap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>")
+	bufmap("n", "grt", "<cmd>lua vim.lsp.buf.type_definition()<cr>")
+	bufmap("n", "grd", "<cmd>lua vim.lsp.buf.declaration()<cr>")
+	bufmap({ "n", "x" }, "gq", "<cmd>lua vim.lsp.buf.format({async = true})<cr>")
 
-  -- Lspsaga
-  bufmap("n", "grr", "<cmd>Lspsaga finder ref<cr>")
-  bufmap("n", "gri", "<cmd>Lspsaga finder imp<cr>")
-  bufmap("n", "grn", "<cmd>Lspsaga rename<cr>")
-  bufmap("n", "gra", "<cmd>Lspsaga code_action<cr>")
-  bufmap("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<cr>")
-  bufmap("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<cr>")
-  bufmap("n", "K", "<cmd>Lspsaga hover_doc<cr>")
+	-- Lspsaga
+	bufmap("n", "grr", "<cmd>Lspsaga finder ref<cr>")
+	bufmap("n", "gri", "<cmd>Lspsaga finder imp<cr>")
+	bufmap("n", "grn", "<cmd>Lspsaga rename<cr>")
+	bufmap("n", "gra", "<cmd>Lspsaga code_action<cr>")
+	bufmap("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<cr>")
+	bufmap("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<cr>")
+	bufmap("n", "K", "<cmd>Lspsaga hover_doc<cr>")
 end
 
 return M
